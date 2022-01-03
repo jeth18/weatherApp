8d86a55d07db1d2ebe4dcb7fbd36c0865a79e110
Jethran Gomez <jethrangomez@MacBook-Pro-de-Jethran.local>
2022-01-03 01:23:55 -0600

    prueba de tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   return (
-    <div>
-
+    <div className='px-0 py-0'>
+      <h1>Name</h1>
+      <div className='container px-8 py-2'>
+        <div className='container w-screen bg-pink'>name</div>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,38 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
+    colors: {
+      'blue': '#6805F2',
+      'pink': '#F1A2F2',
+      'indigo': '#4703A6',
+      'purple': '#4703A6',
+      'purple2': '#260259'
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+      '7': '56px',
+      '8': '64px'
+    }
   },
   plugins: [],
 }
