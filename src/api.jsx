71789d587911a37b774ef17c4a36052a56e6b235
Jethran Gomez <jethrangomez@MacBook-Pro-de-Jethran.local>
2022-01-03 01:03:47 -0600

    add tailwind and api

---
+++ src/api.jsx
@@ -0,0 +1,11 @@
+/* https://www.metaweather.com/api/location/search/?query=tor **/
+/**https://www.metaweather.com/api/location/4118/ */
+
+const api = 'https://www.metaweather.com/api/'
+const apiIcon = 'https://www.metaweather.com/static/img/weather/'
+
+export {
+  api,
+  apiIcon
+} 
+ß
