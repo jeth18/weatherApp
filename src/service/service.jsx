91fca48ea40f4a96e7d50e1e3fe209e8df59ee0b
Jeth18 <jethran.gomez.re@gmail.com>
2022-07-11 14:03:03 -0500

    Cambio de servicios
    Signed-off-by: Jeth18 <jethran.gomez.re@gmail.com>

--- src/service/service.jsx
+++ src/service/service.jsx
@@ -1,11 +1,19 @@
 import axios from 'axios'
 import { api, API_KEY } from '../api'
 
-function getDataByCiudad(ciudad) {
-  return axios
-    .get(`${api}?q=${ciudad}&lang=es&appid=${API_KEY}&units=metric`)
-    .then((response) => response)
-    .catch((error) => error.response)
+async function getDataByCiudad(ciudad) { 
+  let response;
+  try {
+    response = await axios.get(`${api}?q=${ciudad}&lang=es&appid=${API_KEY}&units=metric`);
+  } catch(error) {
+    response = error.response;
+  }
+  return response;
+
+  // return axios
+  //   .get(`${api}?q=${ciudad}&lang=es&appid=${API_KEY}&units=metric`)
+  //   .then((response) => response)
+  //   .catch((error) => error.response)
 }
 
 export { getDataByCiudad }
